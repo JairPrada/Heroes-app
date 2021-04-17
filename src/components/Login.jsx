4fda5646ca7e27f9c1474f9b35a0eee82121f036
JairPrada <jairprada9@gmail.com>
2021-04-17 15:24:55 -0500

    Inicio de Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,3 +1,4 @@
+import { Button } from 'antd';
 import React, { Fragment, useContext } from 'react';
 import { AuthContext } from '../auth/AuthContext';
 import { types } from '../types/types';
@@ -5,19 +6,25 @@ import { types } from '../types/types';
 const Login = ({ history }) => {
     const { dispatch } = useContext(AuthContext)
     const ultimaRuta = localStorage.getItem('ultimaRuta') || '/'
-    const iniciar = () => {
+    const iniciar = ({ nombre="invitado" }) => {
         dispatch({
             type: types.login,
             payload: {
-                name: 'Jair Prada'
+                name: nombre
             }
         })
         history.replace(ultimaRuta)
     }
     return (
         <Fragment>
-            <h1>Login</h1>
-            <button onClick={iniciar}>Login</button>
+            <div style={{ position: "absolute", height: "100vh", width: "100%", backgroundImage: `url('../../Imagenes/fondo.jpg')`, backgroundSize: "cover", filter: "blur(3px)" }}>
+
+            </div>
+            <div style={{ background: "#0E1215F2", display: "flex", flexDirection: "column", borderRadius: "3px", alignItems: "center", padding: "100px 50px", position: "fixed", zIndex: "99", top: "50%", left: "50%", transform: "translateY(-50%) translateX(-50% )" }}>
+                <h1 className="t">Bienvenido a Heroes App</h1>
+                <br />
+                <Button type="primary" onClick={iniciar}>Ingresar como Invitado</Button>
+            </div>
         </Fragment>
     )
 }
